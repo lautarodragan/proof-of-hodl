cfb45a175c8f4c1c7c4f84ad0307a8b0e544d86f
lautarodragan <lautaro.dragan@gmail.com>
2019-05-14 02:06:00 -0300

    more es6

--- src/messageVerifier.ts
+++ src/messageVerifier.ts
@@ -1,17 +1,17 @@
-import * as ethereumJsUtil from 'ethereumjs-util'
+import { bufferToHex, ecrecover, fromRpcSig, hashPersonalMessage, publicToAddress } from 'ethereumjs-util'
 
 export function verify(address: string, message: string, signature: string) {
-  const msgHash = ethereumJsUtil.hashPersonalMessage(Buffer.from(message))
+  const msgHash = hashPersonalMessage(Buffer.from(message))
 
-  const signatureParams = ethereumJsUtil.fromRpcSig(signature);
-  const publicKey = ethereumJsUtil.ecrecover(
+  const signatureParams = fromRpcSig(signature);
+  const publicKey = ecrecover(
     msgHash,
     signatureParams.v,
     signatureParams.r,
     signatureParams.s
   );
-  const addressBuffer = ethereumJsUtil.publicToAddress(publicKey);
-  const addressVerified = ethereumJsUtil.bufferToHex(addressBuffer);
+  const addressBuffer = publicToAddress(publicKey);
+  const addressVerified = bufferToHex(addressBuffer);
 
   return addressVerified === address
 }
